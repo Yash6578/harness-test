c1883896a43d873e1a674ef8976649807f4ece19
yash6578 <yashchawla6578@gmail.com>
2023-02-13 08:57:51 +0530

    first commit

---
+++ src/main/java/com/java/harnesstest/rest/HomeController.java
@@ -0,0 +1,2 @@
+package com.java.harnesstest.rest;public class HomeController {
+}
---
+++ src/test/java/com/java/harnesstest/HarnessTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.harnesstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarnessTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
